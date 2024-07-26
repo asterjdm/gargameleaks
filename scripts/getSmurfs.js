b631072bd3c37e4ec0f47f2930085d674a08cc64
AsteroidusTv <asteroidus@protonmail.com>
2024-07-26 19:14:45 +0300

    Remove debug help

--- scripts/getSmurfs.js
+++ scripts/getSmurfs.js
@@ -2,7 +2,5 @@ async function getSmurfs(searchQuery, sort) {
   let data = await fetch(
     `api/getSmurfs.php?searchQuery=${searchQuery}&sort=${sort}`
   );
-  console.log(data);
   return data.json();
-
 }
